b850ad3b7c0521d00221ee00bfadf73f3358b1dc
gekudera <myfavouritemann@mail.ru>
2020-11-08 16:20:15 +0300

    initialize project

---
+++ sr1_VectorWork/IVector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sr1_VectorWork
+{
+    interface IVector
+    {
+        public int Size { get; }
+        public List<double> GetAllValues();          //отдает весь вектор
+        public double GetValue(int i);           //отдает i-ое значение вектора
+        public void AddValue(double chisl);      //добавляет значение в конец вектора
+        public void SetValue(int h, double chislo);
+
+    }
+}
---
+++ sr1_VectorWork/SomeMatrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace sr1_VectorWork
+{
+    abstract class SomeMatrix : IMatrix
+    {
+        private int row, col;
+        private IVector[] matr;
+
+        public SomeMatrix(int r, int c)
+        {
+            matr = new IVector[r];
+            this.row = r;
+            this.col = c;
+            for (int i = 0; i < r; i++)
+            {
+                matr[i] = Create(c);
+            }
+        }
+
+        protected abstract IVector Create(int co);
+      
+        public int row_count { get { return row; } }
+        public int column_count { get { return col; } }
+
+        public void SetValue(double chisl, int i, int j)
+        {
+            matr[i].SetValue(j, chisl);
+        }
+
+        public double GetValue(int i, int j)
+        {
+            return (matr[i].GetValue(j));
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("______________________");
+            foreach (var item in matr)
+            {
+                Console.Write("|");
+                for (int i=0; i<item.Size; i++)
+                {
+                    Console.Write("{0,2:00}  ", item.GetValue(i));
+                    
+                }
+                Console.Write("|\n");
+            }
+            Console.WriteLine("______________________");
+        }
+    }
+}
